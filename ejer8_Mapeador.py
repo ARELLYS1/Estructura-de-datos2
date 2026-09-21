f7bb9d0004abbeeb9ae51b1e3e2403bf2adbc4df
ARELLYS1 <apazminoa7@unemi.edu.ec>
2026-09-21 12:35:32 -0500

    actualizado_8

--- ejer8_Mapeador.py
+++ ejer8_Mapeador.py
@@ -38,3 +38,46 @@ print(gp.personas_mayores(18))
 
 
 print("Edad promedio:", gp.edad_promedio())
+
+
+
+
+
+#EJERCICIO# 8.1
+#GESTOR DE PRODUCTOS
+# Tenga un método agregar_producto(nombre, precio) que guarde el producto y su precio en un diccionario.
+# Tenga un método productos_caros(precio_minimo) que retorne una lista con los nombres de los
+# productos cuyo precio sea mayor o igual al precio indicado.
+# Tenga un método precio_promedio() que retorne el promedio de todos los precios
+
+class GestorProductos:
+
+    def __init__(self):
+        self.productos = {}
+
+    def agregar_producto(self, nombre, precio):
+        self.productos[nombre] = precio
+
+    def productos_caros(self, precio_minimo):
+        caros = []
+
+        for nombre, precio in self.productos.items():
+            if precio >= precio_minimo:
+                caros.append(nombre)
+
+        return caros
+
+    def precio_promedio(self):
+        return sum(self.productos.values()) / len(self.productos)
+
+
+gp = GestorProductos()
+
+gp.agregar_producto("Cuaderno", 3)
+gp.agregar_producto("Mochila", 25)
+gp.agregar_producto("Calculadora", 15)
+
+
+print(gp.productos_caros(10))
+
+print("Precio promedio:", gp.precio_promedio())
