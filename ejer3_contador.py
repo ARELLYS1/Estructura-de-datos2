7c3cc81b2eab462f1be7ecea74ebf694de735707
ARELLYS1 <apazminoa7@unemi.edu.ec>
2026-09-21 12:22:36 -0500

    actualizado_3

--- ejer3_contador.py
+++ ejer3_contador.py
@@ -39,3 +39,51 @@ print(analizador.lista)
 
 
 print("Cantidad de palabras únicas:", analizador.contar_palabras())
+
+
+
+
+
+
+
+# EJERCICIO#3.1
+# REGISTRO DE NOMBRES
+
+class RegistroNombres:
+
+    def __init__(self):
+        self.conjunto = set()
+        self.lista = []
+
+    def agregar_nombre(self, nombre):
+        if nombre not in self.conjunto:
+            self.conjunto.add(nombre)
+            self.lista.append(nombre)
+
+    def contar_nombres(self):
+        return len(self.conjunto)
+
+    def agregar_multiples(self, *args):
+        for nombre in args:
+            self.agregar_nombre(nombre)
+
+
+
+registro = RegistroNombres()
+
+
+
+registro.agregar_nombre("Ana")
+registro.agregar_nombre("Pedro")
+registro.agregar_nombre("Ana")
+
+print("Nombres después de agregar individualmente:", registro.lista)
+
+
+
+registro.agregar_multiples("Luis", "Maria", "Pedro", "Carlos")
+
+print("Nombres después de agregar múltiples:", registro.lista)
+
+
+print("Cantidad de nombres únicos:", registro.contar_nombres())
